cc970da94dc948ab7886e5802ad837c9674d5d70
Mirza AasimBaig <aasimmirza01@gmail.com>
2021-11-03 12:37:15 +0530

    testing modified file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def process():
     print("Process is running :D")
+    print("Git check")
+
+
+def addingValues():
+    a = 10
+    b = 20
+    print(a+b)
+
+
+def onemoreProcess():
+    a = "Hello, world"
+    b = 200
+    c = 65
+    print(f"{a}, {b}, {c}")
+
 
 if __name__ == "__main__":
     process()
+    onemoreProcess()
+    addingValues()
